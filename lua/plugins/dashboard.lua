0181816b5999a973eede59562d089db5026f615d
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-04-16 00:47:34 +0200

    disable dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,97 +1,71 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    local inspire = require('inspire')
-    local quote = inspire.get_quote()
-    local centered_quote = inspire.center_text(quote.text, quote.author, 52, 8, 52)
-
-    require('dashboard').setup({
-      theme = 'doom',
-      config = {
-        header = {
-          '',
-          '                        .88888888:.                          ',
-          '                       88888888.88888.                       ',
-          '                     .8888888888888888.                      ',
-          '                     888888888888888888                      ',
-          "                     88' _`88'_  `88888                      ",
-          '                     88 88 88 88  88888                      ',
-          '                     88_88_::_88_:88888                      ',
-          '                     88:::,::,:::::8888                      ',
-          "                     88`:::::::::'`8888                      ",
-          "                    .88  `::::'    8:88.                     ",
-          '                   8888            `8:888.                   ',
-          "                 .8888'             `888888.                 ",
-          "                .8888:..  .::.  ...:'8888888:.              ",
-          "               .8888.'     :'     `'::`88:88888             ",
-          "              .8888        '         `.888:8888.            ",
-          '             888:8         .           888:88888            ',
-          '           .888:88        .:           888:88888:           ',
-          '           8888888.       ::           88:888888            ',
-          '           `.::.888.      ::          .88888888             ',
-          "          .::::::.888.    ::         :::`8888'.:.           ",
-          "         ::::::::::.888   '         .::::::::::::           ",
-          "         ::::::::::::.8    '      .:8::::::::::::.         ",
-          '        .::::::::::::::.        .:888:::::::::::::         ',
-          "        :::::::::::::::88:.__..:88888:::::::::::'          ",
-          "         `'.:::::::::::88888888888.88:::::::::'            ",
-          "               `':::_:' -- '' -'-' `':_::::'`              ",
-          '',
-          unpack(centered_quote), -- Fügt das zentrierte Zitat zum Header hinzu
-        },
-        center = {
-          {
-            icon = '  ',
-            icon_hl = 'Title',
-            desc = 'New file',
-            desc_hl = 'String',
-            key = 'e',
-            key_hl = 'Number',
-            action = 'ene | startinsert',
-          },
-          {
-            icon = '  ',
-            icon_hl = 'Title',
-            desc = 'Find file',
-            desc_hl = 'String',
-            key = 'f',
-            key_hl = 'Number',
-            action = 'cd $HOME/Workspace | Telescope find_files',
-          },
-          {
-            icon = '  ',
-            icon_hl = 'Title',
-            desc = 'Recent',
-            desc_hl = 'String',
-            key = 'r',
-            key_hl = 'Number',
-            action = 'Telescope oldfiles',
-          },
-          {
-            icon = '  ',
-            icon_hl = 'Title',
-            desc = 'Settings',
-            desc_hl = 'String',
-            key = 's',
-            key_hl = 'Number',
-            action = 'e $MYVIMRC | cd %:p:h | split . | wincmd k | pwd',
-          },
-          {
-            icon = '  ',
-            icon_hl = 'Title',
-            desc = 'Quit NVIM',
-            desc_hl = 'String',
-            key = 'q',
-            key_hl = 'Number',
-            action = 'qa',
-          },
-        },
-      },
-    })
-  end,
-  dependencies = {
-    { 'nvim-tree/nvim-web-devicons' },
-    { 'RileyGabrielson/inspire.nvim' },
-  },
+  --   'nvimdev/dashboard-nvim',
+  --   event = 'VimEnter',
+  --   config = function()
+  --     local inspire = require('inspire')
+  --     local quote = inspire.get_quote()
+  --     local centered_quote = inspire.center_text(quote.text, quote.author, 52, 8, 52)
+  --
+  --     require('dashboard').setup({
+  --       theme = 'doom',
+  --       config = {
+  --         header = {
+  --           '',
+  --           '                        .88888888:.                          ',
+  --           '                       88888888.88888.                       ',
+  --           '                     .8888888888888888.                      ',
+  --           '                     888888888888888888                      ',
+  --           "                     88' _`88'_  `88888                      ",
+  --           '                     88 88 88 88  88888                      ',
+  --           '                     88_88_::_88_:88888                      ',
+  --           '                     88:::,::,:::::8888                      ',
+  --           "                     88`:::::::::'`8888                      ",
+  --           "                    .88  `::::'    8:88.                     ",
+  --           '                   8888            `8:888.                   ',
+  --           "                 .8888'             `888888.                 ",
+  --           "                .8888:..  .::.  ...:'8888888:.               ",
+  --           "               .8888.'     :'     `'::`88:88888              ",
+  --           "              .8888        '         `.888:8888.             ",
+  --           '             888:8         .           888:88888             ',
+  --           '           .888:88        .:           888:88888:            ',
+  --           '           8888888.       ::           88:888888             ',
+  --           '           `.::.888.      ::          .88888888              ',
+  --           "          .::::::.888.    ::         :::`8888'.:.            ",
+  --           "         ::::::::::.888   '         .::::::::::::            ",
+  --           "         ::::::::::::.8    '      .:8::::::::::::.           ",
+  --           '        .::::::::::::::.        .:888:::::::::::::           ',
+  --           "        :::::::::::::::88:.__..:88888:::::::::::'            ",
+  --           "         `'.:::::::::::88888888888.88:::::::::'              ",
+  --           "               `':::_:' -- '' -'-' `':_::::'`                ",
+  --           '',
+  --           require('inspire').centered_quote(), -- Fügt das zentrierte Zitat zum Header hinzu
+  --         },
+  --         center = {
+  --           icon = ' ',
+  --           icon_hl = 'Title',
+  --           desc = 'Find File           ',
+  --           desc_hl = 'String',
+  --           key = 'b',
+  --           keymap = 'SPC f f',
+  --           key_hl = 'Number',
+  --           key_format = ' %s', -- remove default surrounding `[]`
+  --           action = 'lua print(2)',
+  --         },
+  --         {
+  --           icon = ' ',
+  --           desc = 'Find Dotfiles',
+  --           key = 'f',
+  --           keymap = 'SPC f d',
+  --           key_format = ' %s', -- remove default surrounding `[]`
+  --           action = 'lua print(3)',
+  --         },
+  --         footer = {},
+  --         vertical_center = true, -- Center the Dashboard on the vertical (from top to bottom)
+  --       },
+  --     })
+  --   end,
+  --   dependencies = {
+  --     { 'nvim-tree/nvim-web-devicons' },
+  --     { 'RileyGabrielson/inspire.nvim' },
+  --   },
 }
