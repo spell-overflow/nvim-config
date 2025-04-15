9b7110f8e64e03e451e1b8cac5528e2744c67ed3
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-04-15 19:25:16 +0200

    add makro recording to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,18 +10,18 @@
 --}
 
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   config = function()
     local mode = {
-      'mode',
+      "mode",
       fmt = function(str)
-        return ' ' .. str
+        return " " .. str
         -- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
       end,
     }
 
     local filename = {
-      'filename',
+      "filename",
       file_status = true, -- displays file status (readonly status, modified status)
       path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
     }
@@ -31,10 +31,10 @@ return {
     end
 
     local diagnostics = {
-      'diagnostics',
-      sources = { 'nvim_diagnostic' },
-      sections = { 'error', 'warn' },
-      symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      sections = { "error", "warn" },
+      symbols = { error = " ", warn = " ", info = " ", hint = " " },
       colored = false,
       update_in_insert = false,
       always_visible = false,
@@ -42,43 +42,62 @@ return {
     }
 
     local diff = {
-      'diff',
+      "diff",
       colored = false,
-      symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+      symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
       cond = hide_in_width,
     }
 
-    require('lualine').setup {
+    -- Neue Komponente für Makro-Status
+    local macro_recording = {
+      function()
+        local recording_register = vim.fn.reg_recording()
+        if recording_register == "" then
+          -- Wenn wir gerade einen @-Befehl ausführen
+          local count = vim.v.count
+          local macro_register = vim.v.reg
+          if count > 0 and macro_register ~= "" then
+            return count .. "@" .. macro_register
+          end
+          return ""
+        else
+          return "Recording @" .. recording_register
+        end
+      end,
+      color = { fg = "#ff9e64" },
+    }
+
+    require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = 'catppuccin', -- Set theme based on environment variable
+        theme = "catppuccin", -- Set theme based on environment variable
         -- Some useful glyphs:
         -- https://www.nerdfonts.com/cheat-sheet
         --        
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        disabled_filetypes = { 'alpha', 'neo-tree', 'explorer' },
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        disabled_filetypes = { "alpha", "neo-tree", "explorer" },
         always_divide_middle = true,
+        globalstatus = true,
       },
       sections = {
         lualine_a = { mode },
-        lualine_b = { 'branch' },
-        lualine_c = { filename },
-        lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
-        lualine_y = { 'location' },
-        lualine_z = { 'progress' },
+        lualine_b = { "branch" },
+        lualine_c = { filename, macro_recording },
+        lualine_x = { diagnostics, diff, { "encoding", cond = hide_in_width }, { "filetype", cond = hide_in_width } },
+        lualine_y = { "location" },
+        lualine_z = { "progress" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { { 'location', padding = 0 } },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { { "location", padding = 0 } },
         lualine_y = {},
         lualine_z = {},
       },
---      tabline = {},
---      extensions = { 'fugitive' },
-    }
+      --      tabline = {},
+      --      extensions = { 'fugitive' },
+    })
   end,
 }
-
