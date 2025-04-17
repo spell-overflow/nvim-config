fcff25a18178b7cc11dcaa2992eb324ff8497d5d
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-04-17 15:37:27 +0200

    remove go to Declaration
    from lspconfig since it's not relevant for my usecases

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -81,7 +81,7 @@ return {
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
-        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        -- map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
