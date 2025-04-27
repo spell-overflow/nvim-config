ea757015391852232c76dc495652acb1c2f6c61c
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-04-27 04:25:13 +0200

    add scrollbar

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -6,6 +6,21 @@ return {
       handlers = {
         gitsigns = true,
         search = true,
+        -- ale = false,
+        -- handle = true,
+        -- cursor = true,
+        -- diganostic = true,
+      },
+      excluded_filetypes = {
+        'dropbar_menu',
+        'dropbar_menu_fzf',
+        'DressingInput',
+        'cmp_docs',
+        'cmp_menu',
+        'noice',
+        'prompt',
+        'TelescopePrompt',
+        'snacks_picker_list',
       },
     })
     require('gitsigns').setup()
