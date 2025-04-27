607832b14095bd94e601cdec22942b1f646c110d
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-04-27 03:25:37 +0200

    add inc-rename to rename variables

---
+++ lua/plugins/inc-rename.lua
@@ -0,0 +1,8 @@
+return {
+  'smjonas/inc-rename.nvim',
+  config = function()
+    require('inc_rename').setup({
+      vim.keymap.set('n', '<leader>ir', ':IncRename ', { desc = 'Rename Variable' }),
+    })
+  end,
+}
