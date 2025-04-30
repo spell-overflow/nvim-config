296a81f4a15bf56000aafb5b2635a4a0f6089e7f
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-04-30 14:32:32 +0200

    fix lualine
    show filetype icon
    declutter config-file

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,123 +1,68 @@
---return {
---  "nvim-lualine/lualine.nvim",
---  config = function()
---    require('lualine').setup({
---      options = {
---        theme = "catppuccin"
---      }
---    })
---  end
---}
-
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'folke/trouble.nvim', 'nvim-web-devicons' },
   config = function()
-    local mode = {
-      'mode',
-      fmt = function(str)
-        return ' ' .. str
-        -- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
+    -- Setup Spinner Symbols
+    local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+
+    -- Spinner State
+    vim.g.code_companion_processing = false
+    vim.g.code_companion_spinner_index = 1
+
+    -- Autocommands to manage processing state
+    vim.api.nvim_create_augroup('CodeCompanionLualine', { clear = true })
+
+    vim.api.nvim_create_autocmd('User', {
+      group = 'CodeCompanionLualine',
+      pattern = 'CodeCompanionRequestStarted',
+      callback = function()
+        vim.g.code_companion_processing = true
       end,
-    }
+    })
 
-    local filename = {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
-    }
+    vim.api.nvim_create_autocmd('User', {
+      group = 'CodeCompanionLualine',
+      pattern = 'CodeCompanionRequestFinished',
+      callback = function()
+        vim.g.code_companion_processing = false
+      end,
+    })
 
-    local hide_in_width = function()
+    -- CodeCompanion Component
+    local function code_companion()
+      if vim.g.code_companion_processing then
+        vim.g.code_companion_spinner_index = (vim.g.code_companion_spinner_index % #spinner) + 1
+        return spinner[vim.g.code_companion_spinner_index]
+      end
+      return ''
+    end
+
+    -- Macro recording display
+    local function macro_recording()
+      local reg = vim.fn.reg_recording()
+      return reg ~= '' and 'Recording @' .. reg or ''
+    end
+
+    -- Hide on narrow windows
+    local function hide_in_width()
       return vim.fn.winwidth(0) > 100
     end
 
-    local diagnostics = {
-      'diagnostics',
-      sources = { 'nvim_diagnostic' },
-      sections = { 'error', 'warn' },
-      symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-      colored = false,
-      update_in_insert = false,
-      always_visible = false,
-      cond = hide_in_width,
-    }
+    -- Setup Trouble LSP symbols
+    local trouble = require('trouble')
+    local symbols = trouble.statusline({
+      mode = 'lsp_document_symbols',
+      title = false,
+      filter = { range = true },
+      format = '{kind_icon}{symbol.name:Normal}',
+      hl_group = 'lualine_c_normal',
+    })
 
-    local diff = {
-      'diff',
-      colored = false,
-      symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
-      cond = hide_in_width,
-    }
-
-    local macro_recording = {
-      function()
-        local recording_register = vim.fn.reg_recording()
-        if recording_register == '' then
-          local count = vim.v.count
-          local macro_register = vim.v.reg
-          if count > 0 and macro_register ~= '' then
-            return count .. '@' .. macro_register
-          end
-          return ''
-        else
-          return 'Recording @' .. recording_register
-        end
-      end,
-      color = { fg = '#ff9e64' },
-    }
-
-    local CodeCompanion = require('lualine.component'):extend()
-
-    CodeCompanion.processing = false
-    CodeCompanion.spinner_index = 1
-
-    local spinner_symbols = {
-      '⠋',
-      '⠙',
-      '⠹',
-      '⠸',
-      '⠼',
-      '⠴',
-      '⠦',
-      '⠧',
-      '⠇',
-      '⠏',
-    }
-    local spinner_symbols_len = 10
-
-    function CodeCompanion:init(options)
-      CodeCompanion.super.init(self, options)
-
-      local group = vim.api.nvim_create_augroup('CodeCompanionHooks', {})
-
-      vim.api.nvim_create_autocmd({ 'User' }, {
-        pattern = 'CodeCompanionRequest*',
-        group = group,
-        callback = function(request)
-          if request.match == 'CodeCompanionRequestStarted' then
-            self.processing = true
-          elseif request.match == 'CodeCompanionRequestFinished' then
-            self.processing = false
-          end
-        end,
-      })
-    end
-
-    function CodeCompanion:update_status()
-      if self.processing then
-        self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
-        return spinner_symbols[self.spinner_index]
-      else
-        return nil
-      end
-    end
-
+    -- Setup lualine
     require('lualine').setup({
       options = {
+        theme = 'catppuccin',
         icons_enabled = true,
-        theme = 'catppuccin', -- Set theme based on environment variable
-        -- Some useful glyphs:
-        -- https://www.nerdfonts.com/cheat-sheet
-        --        
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = { 'alpha', 'neo-tree', 'explorer' },
@@ -125,18 +70,73 @@ return {
         globalstatus = true,
       },
       sections = {
-        lualine_a = { mode },
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(str)
+              return ' ' .. str
+            end,
+          },
+        },
         lualine_b = { 'branch' },
-        lualine_c = { filename, macro_recording },
+        lualine_c = {
+          {
+            'filename',
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
+            path = 0, -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+              modified = '[+]', -- Text to show when the file is modified.
+              readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+              unnamed = '[No Name]', -- Text to show for unnamed buffers.
+              newfile = '[New]', -- Text to show for newly created file before first write}
+            },
+          },
+          macro_recording,
+          -- { symbols.get, cond = symbols.has },
+          { 'searchcount', maxcount = 999, timeout = 500 },
+        },
+
         lualine_x = {
-          CodeCompanion,
-          diagnostics,
-          diff,
+          code_companion,
+          {
+            'diagnostics',
+            sources = { 'nvim_diagnostic' },
+            sections = { 'error', 'warn' },
+            symbols = { error = ' ', warn = ' ' },
+            cond = hide_in_width,
+          },
+          {
+            'diff',
+            symbols = { added = ' ', modified = ' ', removed = ' ' },
+            cond = hide_in_width,
+          },
           { 'encoding', cond = hide_in_width },
-          { 'filetype', cond = hide_in_width },
+          {
+            'fileformat',
+            symbols = {
+              unix = '', -- e712
+              dos = '', -- e70f
+              mac = '', -- e711
+            },
+          },
+          {
+            'filetype',
+            colored = true, -- Displays filetype icon in color if set to true
+            icon_only = false, -- Display only an icon for filetype
+            icon = { align = 'right' }, -- Display filetype icon on the right hand side
+            -- icon =    {'X', align='right'}
+            -- Icon string ^ in table is ignored in filetype component,
+          },
         },
         lualine_y = { 'location' },
-        lualine_z = { 'progress' },
+        lualine_z = { '%p%%/%L' },
       },
       inactive_sections = {
         lualine_a = {},
@@ -146,28 +146,6 @@ return {
         lualine_y = {},
         lualine_z = {},
       },
-      --      tabline = {},
-      --      extensions = { 'fugitive' },
-      --
-    }, {
-      'nvim-lualine/lualine.nvim',
-      opts = function(_, opts)
-        local trouble = require('trouble')
-        local symbols = trouble.statusline({
-          mode = 'lsp_document_symbols',
-          groups = {},
-          title = false,
-          filter = { range = true },
-          format = '{kind_icon}{symbol.name:Normal}',
-          -- The following line is needed to fix the background color
-          -- Set it to the lualine section you want to use
-          hl_group = 'lualine_c_normal',
-        })
-        table.insert(opts.sections.lualine_c, {
-          symbols.get,
-          cond = symbols.has,
-        })
-      end,
     })
   end,
 }
