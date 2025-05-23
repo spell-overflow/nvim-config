28c242de33cadbf1375326d2a0223545f86dc2b5
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-05-23 17:04:06 +0200

    refactor telescope config and add keymap to open results in trouble

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,24 +2,34 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+      'folke/trouble.nvim',
+    },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    end,
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require('telescope').setup({
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown({}),
+      local telescope = require('telescope')
+      local actions = require('telescope.actions')
+      local open_with_trouble = require('trouble.sources.telescope').open_with_trouble
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            n = {
+              ['<C-t>'] = open_with_trouble,
+              ['<C-h>'] = actions.move_selection_previous,
+              ['<C-l>'] = actions.move_selection_next,
+            },
+          },
+          extensions = {
+            ['ui-select'] = require('telescope.themes').get_dropdown({}),
           },
         },
       })
-      require('telescope').load_extension('ui-select')
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end,
   },
 }
