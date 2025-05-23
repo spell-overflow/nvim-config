4e430f885cba24b4aa64b06b3267c5087f96e3de
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-05-23 23:17:08 +0200

    add done keyword to todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -15,12 +15,14 @@ return {
         alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
         -- signs = false, -- configure signs for some keywords individually
       },
-      TODO = { icon = ' ', color = 'info' },
+      -- TODO = { icon = ' ', color = 'info' },
+      TODO = { icon = ' ', color = 'warning' },
       HACK = { icon = ' ', color = 'warning' },
       WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
       PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
       NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
       TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+      DONE = { icon = ' ',  color = 'info'}
     },
     gui_style = {
       fg = 'NONE', -- The gui style to use for the fg highlight group.
@@ -36,7 +38,7 @@ return {
       multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
       before = '', -- "fg" or "bg" or empty
-      keyword = 'wide', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      keyword = 'bg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
       after = 'fg', -- "fg" or "bg" or empty
       pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
