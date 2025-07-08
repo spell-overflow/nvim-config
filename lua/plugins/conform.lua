fb3cf1e5b4bcf82b85d73f862f7d66fcd7398979
Anneke Hugenberg <spelloverflow@annekehugenberg.de>
2025-07-08 16:53:23 +0200

    increase timeout for prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -24,7 +24,7 @@ return {
           return nil
         else
           return {
-            timeout_ms = 1000,
+            timeout_ms = 2000,
             lsp_format = 'fallback',
           }
         end
